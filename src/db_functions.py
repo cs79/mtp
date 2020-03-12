561c3035f0c6dc896a7d721ac294191ef9e6dade
Alex <cloudstrife79@gmail.com>
2020-03-12 18:32:20 -0400

    add yaml config file for use with new db convenience functions

---
+++ src/db_functions.py
@@ -0,0 +1,22 @@
+# convenience functions for querying info from database
+
+# imports
+import yaml
+import sqlite3
+import pandas as pd
+
+# configuration
+cfg = yaml.load(open('../config/db_config.yaml'), Loader=yaml.SafeLoader)
+conn = sqlite3.connect('../config/{}'.format(cfg['DB_NAME']))
+
+# functions
+def get_user_data(conn=conn, tb_name=cfg['LEDGER_USER_TABLE_NAME']):
+    qry = 'SELECT UserID, LedgerID, LedgerUserID FROM {}'.format(tb_name)
+    return pd.read_sql(qry, conn)
+
+def get_max_userid(conn=conn, tb_name=cfg['USER_TABLE_NAME']):
+    qry = 'SELECT MAX(UserID) FROM {}'.format(tb_name)
+    res = pd.read_sql(qry, conn).iloc[0,0]
+    if res is None:
+        return 0
+    return res
