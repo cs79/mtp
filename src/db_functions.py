7df20debc79f63b2378e0062f7179d3f8a072ba8
Alex <cloudstrife79@gmail.com>
2020-03-16 14:30:37 -0400

    remove default connection binding to avoid cache errors

--- src/db_functions.py
+++ src/db_functions.py
@@ -10,18 +10,18 @@ cfg = yaml.load(open('../config/db_config.yaml'), Loader=yaml.SafeLoader)
 conn = sqlite3.connect('../config/{}'.format(cfg['DB_NAME']))
 
 # functions
-def get_user_data(conn=conn, tb_name=cfg['LEDGER_USER_TABLE_NAME']):
+def get_user_data(conn, tb_name=cfg['LEDGER_USER_TABLE_NAME']):
     qry = 'SELECT UserID, LedgerID, LedgerUserID FROM {}'.format(tb_name)
     return pd.read_sql(qry, conn)
 
-def get_max_userid(conn=conn, tb_name=cfg['USER_TABLE_NAME']):
+def get_max_userid(conn, tb_name=cfg['USER_TABLE_NAME']):
     qry = 'SELECT MAX(UserID) FROM {}'.format(tb_name)
     res = pd.read_sql(qry, conn).iloc[0,0]
     if res is None:
         return 0
     return res
 
-def get_ledgerids(conn=conn, tb_name=cfg['LEDGER_TABLE_NAME'], lname=None):
+def get_ledgerids(conn, tb_name=cfg['LEDGER_TABLE_NAME'], lname=None):
     if lname is None:
         qry = 'SELECT * FROM {}'.format(tb_name)
     else:
